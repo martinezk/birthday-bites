04dd106cc5f03808e588a735693d5c35afbb9c75
Kristina Martinez <misskristinamartinez@gmail.com>
2017-10-07 09:18:04 -0400

    login page

---
+++ src/components/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <section id="login">
+    <div class="container">
+      <div class="row">
+        <div class="center col-md-6">
+          <div class="form-wrap">
+            <h1>Login</h1>
+            <form role="form" id="login-form" autocomplete="off">
+              <div class="form-group">
+                <label for="email" class="sr-only">Email</label>
+                <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.io">
+              </div>
+              <div class="form-group">
+                <label for="key" class="sr-only">Password</label>
+                <input type="password" name="key" id="key" class="form-control" placeholder="Password">
+              </div>
+              <input type="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Log in">
+            </form>
+            <a href="javascript:;" class="forget" >Forgot your password?</a>
+            <hr>
+          </div>
+        </div>
+        <!-- /.col-xs-12 -->
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container -->
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {
+      msg: 'Hello'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.center {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+
+</style>
--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import Hello from '@/components/Hello'
+import Login from '@/components/Login'
 
-describe('Hello.vue', () => {
+describe('Login.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello)
+    const Constructor = Vue.extend(Login)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
+    expect(vm.$el.querySelector('.login h1').textContent)
       .to.equal('Welcome to Your Vue.js App')
   })
 })
