38313983c5e684f542ec5a6d6c8087beedf17c1d
Kristina Martinez <misskristinamartinez@gmail.com>
2017-10-09 22:11:11 -0400

    changed date format

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,24 +1,20 @@
 <template>
   <div id="search" class="container">
-    <div class="row date">
-      <div class="col-md-6">
-        <form class="form">
-          <div class="form-group">
-            <div class="input-group">
-              <span class="input-group-addon" id="month-addon">Month: </span>
-              <select v-model="month" name="month" class="form-control" aria-describedby="month-addon">
-                <option v-for="month in 12" v-bind:key="month.index">{{ month }}</option>
-              </select>
-              <span class="input-group-addon" id="day-addon">Day: </span>
-              <select v-model="day" name="day" class="form-control" aria-describedby="day-addon">
-                <option v-for="day in 31" v-bind:key="day.index">{{ day }}</option>
-              </select>
-              <button @click="searchButton" class="btn btn-primary">Search</button>
+    <div class="row">
+      <div class="col-md-4 center">
+        <form class="form-inline">
+          <div class="form-group row">
+            <label for="example-date-input" class="col-2 col-form-label">Date</label>
+            <div class="col-10">
+              <input v-model="birthDay" class="form-control" type="date" value="2017-01-01" max="2017-09-30">
             </div>
           </div>
+          <button class="btn btn-primary">Search</button>
         </form>
       </div>
     </div>
+    <hr />
+    <span>{{ results }}</span>
     <div class="row">
       <div class="col-sm-4" v-bind:key="result.index" v-for="result in results">
         <div class="card">
@@ -34,51 +30,26 @@
 </template>
 
 <script>
-import axios from 'axios';
-const FDABaseUrl = "https://api.fda.gov/food/enforcement.json?search=report_date:[";
-function buildUrl(month, day) {
-  return FDABaseUrl + 2016 + month + day + "]&limit=6"
-}
-
 export default {
   data() {
     return {
-      results: [],
-      months: [],
-      month: '1',
-      day: '1'
+      birthDay: '',
+      results: ''
     }
   },
-  mounted() {
-    /*this is not supposed to run on page load but I have it here to test whether the values are working*/
-    this.getItems(this.month + this.day);
+  watch: {
+    birthDay: function() {
+      this.results = ''
+      if (this.birthDay.length == 10) {
+        this.fetchItems()
+      }
+    }
   },
   methods: {
-    getItems(search) {
-      let url = buildUrl(search);
-      axios.get(url).then((response) => {
-        this.results = response.data.results;
-      }).catch(error => { console.log(error); });
-    },
-    searchButton: function(event) {
-      console.log(search);
-
-    },
-    /*date: "recall_initiation_date"*/
-  },
-  computed: {
-    formatDate: function(date) {
-      /*have not used this function yet but trying to connect to to what I pass into getItems  */
-      let d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = 2016;
-
-      if (month.length < 2) month = '0' + month;
-      if (day.length < 2) day = '0' + day;
-
-      return [2016, month, day].join('');
+    fetchItems: function() {
+      this.results = "Searching..."
     }
+
   }
 }
 </script>
@@ -93,7 +64,16 @@ export default {
   margin-top: 60px;
 }
 
+.center {
+  margin-left: auto;
+  margin-right: auto;
+}
+
 .card {
   height: 200px;
+}
+
+.inline {
+  display: inline-block;
 }
 </style>
